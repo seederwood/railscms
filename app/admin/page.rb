d17b94a2cf30630f98148a1973e55eb0dc4094eb
Ray Climie <seederwood@gmail.com>
2017-07-21 09:48:47 -0300

    setup blocks and admin dashboard pages

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -27,7 +27,7 @@ ActiveAdmin.register Page do
     f.inputs 'Details' do
       f.input :title, label: 'Title'
       f.input :section, label: 'Section'
-      f.input :body, label: 'Body'
+      f.input :body, as: :html_editor, label: 'Body'
       f.input :order, label: 'Order'
       f.input :is_published, label: 'Published'
       f.input :menu_display, label: 'Display In Menu'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721014413) do
+ActiveRecord::Schema.define(version: 20170721122531) do
 
   create_table "active_admin_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "namespace"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20170721014413) do
     t.boolean "is_published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "order"
   end
 
   create_table "pages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
